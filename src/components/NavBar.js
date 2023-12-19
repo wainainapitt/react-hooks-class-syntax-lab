c1f7aaf9dbededaa428596c8737c55529bc40e1f
peter <pngugi.wainaina@gmail.com>
2023-12-20 00:48:18 +0300

    lab done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-    </nav>
-  );
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+      </nav>
+    );
+  }
 }
 
 export default NavBar;
